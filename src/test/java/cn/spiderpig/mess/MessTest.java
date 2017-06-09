fbaac7e8bcf99d436d9533cb9e4972e1590b855a
ScvQ <905522052@qq.com>
2017-06-09 18:08:11 +0800

    2017-06-09 18:08:07

--- src/test/java/cn/spiderpig/mess/MessTest.java
+++ src/test/java/cn/spiderpig/mess/MessTest.java
@@ -1,15 +1,36 @@
 package cn.spiderpig.mess;
 
-import junit.framework.TestCase;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class MessTest extends TestCase{
-    
-    public void mess(){
-        String str = "a,b,c,,";
+import org.junit.Test;
+
+public class MessTest {
+
+    @Test
+    public void mess() {
+        String str = "a,b,c,e,f";
         String[] ary = str.split(",");
-        //预期大于3，结果是3
-        String s = str.substring(str.lastIndexOf(","), str.lastIndexOf(",")+1);
+        String s = str.substring(str.lastIndexOf(",")+1, str.lastIndexOf(",") + 2);
         System.out.println(s);
+    }
+
+    @Test
+    public void testI18N() {
+        ResourceBundle rb = ResourceBundle.getBundle("i18n");
+        String name = MessageFormat.format(rb.getString("name"), new String[] { "1234" });
+        String nickname = MessageFormat.format(rb.getString("name"), new String[] { "1234" });
+
+        System.out.println(name);
+        System.out.println(nickname);
+
+        ResourceBundle rb2 = ResourceBundle.getBundle("i18n", new Locale("en_US"));
+        String name2 = MessageFormat.format(rb2.getString("name"), new String[] { "1234" });
+        String nickname2 = MessageFormat.format(rb2.getString("name"), new String[] { "1234" });
+
+        System.out.println(name2);
+        System.out.println(nickname2);
     }
 
 }
